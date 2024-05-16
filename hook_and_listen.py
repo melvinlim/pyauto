98181c67ac2afd93d8b342b4d2fbfe7cfab65f17
melvinlim <dxlim@outlook.com>
2024-05-16 11:06:19 -0700

    put hook code into a class.

--- hook_and_listen.py
+++ hook_and_listen.py
@@ -1,44 +1,43 @@
 import win32api
 import win32con
-from threading import Timer
 from pywinauto.win32_hooks import Hook
 from pywinauto.win32_hooks import KeyboardEvent
 from pywinauto.win32_hooks import MouseEvent
 
-def on_timer():
-    """Callback by timer out"""
-    win32api.PostThreadMessage(main_thread_id, win32con.WM_QUIT, 0, 0);
+class hook(object):
+    def __init__(self):
+        self.main_thread_id = win32api.GetCurrentThreadId()
+        def on_event(args):
+            """Callback for keyboard and mouse events"""
+            if isinstance(args, KeyboardEvent):
+                if args.current_key == 'A' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
+                    print("Ctrl + A was pressed");
+                    
+                if args.current_key == 'L' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
+                    print("Ctrl + L was pressed");
+                    print('quitting')
+                    print('press CTRL-D to close Idle window.')
+                    win32api.PostThreadMessage(self.main_thread_id, win32con.WM_QUIT, 0, 0);
 
-def on_event(args):
-    """Callback for keyboard and mouse events"""
-    if isinstance(args, KeyboardEvent):
-        if args.current_key == 'A' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
-            print("Ctrl + A was pressed");
-            
-        if args.current_key == 'L' and args.event_type == 'key down' and 'Lcontrol' in args.pressed_key:
-            print("Ctrl + L was pressed");
+                if args.current_key == 'K' and args.event_type == 'key down':
+                    print("K was pressed");
 
-        if args.current_key == 'K' and args.event_type == 'key down':
-            print("K was pressed");
+                if args.current_key == 'M' and args.event_type == 'key down' and 'U' in args.pressed_key:
+                    hk.unhook_mouse()
+                    print("Unhook mouse")
 
-        if args.current_key == 'M' and args.event_type == 'key down' and 'U' in args.pressed_key:
-            hk.unhook_mouse()
-            print("Unhook mouse")
+                if args.current_key == 'K' and args.event_type == 'key down' and 'U' in args.pressed_key:
+                    hk.unhook_keyboard()
+                    print("Unhook keyboard")
 
-        if args.current_key == 'K' and args.event_type == 'key down' and 'U' in args.pressed_key:
-            hk.unhook_keyboard()
-            print("Unhook keyboard")
+            if isinstance(args, MouseEvent):
+                if args.current_key == 'RButton' and args.event_type == 'key down':
+                    print ("Right button pressed")
 
-    if isinstance(args, MouseEvent):
-        if args.current_key == 'RButton' and args.event_type == 'key down':
-            print ("Right button pressed")
+                if args.current_key == 'WheelButton' and args.event_type == 'key down':
+                    print("Wheel button pressed")
+        hk = Hook()
+        hk.handler = on_event
+        hk.hook(keyboard=True, mouse=True)
 
-        if args.current_key == 'WheelButton' and args.event_type == 'key down':
-            print("Wheel button pressed")
-
-hk = Hook()
-hk.handler = on_event
-main_thread_id = win32api.GetCurrentThreadId()
-t = Timer(3600, on_timer)  # Quit after 1 hour
-t.start()
-hk.hook(keyboard=True, mouse=True)
+h = hook()
