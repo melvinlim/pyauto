106a367dd3035cd28db3e538f07afc66490b5f44
melvinlim <dxlim@outlook.com>
2024-05-16 15:04:46 -0700

    put screen writing stuff in a class.

--- screenwrite.py
+++ screenwrite.py
@@ -13,34 +13,47 @@ import win32con
 import win32gui
 import pywintypes
 
-root=Tkinter.Tk()
-text=Tkinter.StringVar()
-text.set('Text variable\nasdf')
+def nop(self):
+    pass
 
-#modified from https://stackoverflow.com/questions/21840133/how-to-display-text-on-the-screen-without-a-window-using-python
-label = Tkinter.Label(root,
-    textvariable=text,
-    #text='Text on the screen',
-    font=('Times New Roman','80'),
-    fg='gray',
-    bg='white')
-label.master.overrideredirect(True)
-label.master.geometry("+250+250")
-label.master.lift()
-label.master.wm_attributes("-topmost", True)
-label.master.wm_attributes("-disabled", True)
-label.master.wm_attributes("-transparentcolor", "white")
+class ScreenWriter(object):
+    def __init__(self,callback=None):
+        root=Tkinter.Tk()
+        self.text=Tkinter.StringVar()
+        #text.set('Text variable\nasdf')
 
-hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
-# http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
-# The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
-exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+        #modified from https://stackoverflow.com/questions/21840133/how-to-display-text-on-the-screen-without-a-window-using-python
+        label = Tkinter.Label(root,
+            textvariable=self.text,
+            #text='Text on the screen',
+            font=('Times New Roman','80'),
+            fg='gray',
+            bg='white')
+        label.master.overrideredirect(True)
+        label.master.geometry("+250+250")
+        label.master.lift()
+        label.master.wm_attributes("-topmost", True)
+        label.master.wm_attributes("-disabled", True)
+        label.master.wm_attributes("-transparentcolor", "white")
 
-label.pack()
-#label.mainloop()
+        hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
+        # http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+        # The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+        exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+        win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
 
-while True:
-    text.set(getTime())
-    root.update_idletasks()
-    root.update()
+        label.pack()
+        #label.mainloop()
+
+        if(not callback):
+            callback=nop
+
+        while True:
+            callback(self)
+            root.update_idletasks()
+            root.update()
+
+def callback(self):
+    self.text.set(getTime())
+
+sw=ScreenWriter(callback)
