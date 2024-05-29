9194f9582990decbb9f6e014a0994cfec6c8b057
melvinlim <dxlim@outlook.com>
2024-05-29 11:04:36 -0700

    tried to make it modular.

--- animategif.py
+++ animategif.py
@@ -17,6 +17,7 @@ def nop(self):
 #modified from https://www.blog.pythonlibrary.org/2023/12/05/viewing-an-animated-gif-with-python/
 class MyLabel(Tkinter.Label):
     def __init__(self, master, filename):
+        
         im = Image.open(filename)
         seq =  []
         try:
@@ -38,6 +39,28 @@ class MyLabel(Tkinter.Label):
             self.frames.append(ImageTk.PhotoImage(frame))
         self.idx = 0
         self.cancel = self.after(self.delay, self.play)
+
+        #modified from https://stackoverflow.com/questions/21840133/how-to-display-text-on-the-screen-without-a-window-using-python
+        self.anchor='w'
+        self.justify='left'
+        self.fg='gray'
+        self.bg='white'
+        self.master.overrideredirect(True)
+        self.master.geometry("+0+100")     #x and y locations
+        self.master.lift()
+        self.master.wm_attributes("-topmost", True)
+        self.master.wm_attributes("-disabled", True)
+        self.master.wm_attributes("-transparentcolor", "white")
+
+        hWindow = pywintypes.HANDLE(int(self.master.frame(), 16))
+        # http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+        # The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
+        exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+        win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+
+        self.pack()
+        #self.mainloop()
+        
     def play(self):
         self.config(image=self.frames[self.idx])
         self.idx += 1
@@ -53,36 +76,14 @@ class ScreenWriter(object):
         #gif_path="C:\\Users\\winuser\\Downloads\\R.gif"
         gif_path="R.gif"
 
-        #modified from https://stackoverflow.com/questions/21840133/how-to-display-text-on-the-screen-without-a-window-using-python
         label = MyLabel(root,
                         gif_path
             )
-        label.anchor='w'
-        label.justify='left'
-        label.fg='gray'
-        label.bg='white'
-        label.master.overrideredirect(True)
-        #label.master.geometry("+250+250")
-        #label.master.geometry("+0+0")
-        label.master.geometry("+0+100")     #x and y locations
-        label.master.lift()
-        label.master.wm_attributes("-topmost", True)
-        label.master.wm_attributes("-disabled", True)
-        label.master.wm_attributes("-transparentcolor", "white")
-
-        hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
-        # http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
-        # The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
-        exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-        win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
-
-        label.pack()
-        #label.mainloop()
 
         if(not callback):
             callback=nop
 
-        while True:
+        while False:
             #label.play()
             cb=callback(self)
             if(cb):
